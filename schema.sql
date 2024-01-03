0ba1a7d7c121f53eb58547b7982a0e42b42ba835
typingmonk <tmonk.hsieh@tmonk.tw>
2024-01-03 16:19:20 +0800

    Create sqlite table schema.

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE bill (
+  id INTEGER PRIMARY KEY,
+  term INTEGER,
+  session_period INTEGER,
+  bill_type TEXT,
+  proposal_source TEXT,
+  ppg_bill_number TEXT,
+  serial_number TEXT
+);
+
+CREATE TABLE bill_state (
+  id INTEGER PRIMARY KEY,
+  state_name TEXT,
+  host TEXT
+);
+
+INSERT INTO bill_state (state_name, host) VALUES ('start', '');
+INSERT INTO bill_state (state_name, host) VALUES ('end', '');
+
+CREATE TABLE progress_link (
+  id INTEGER PRIMARY KEY,
+  bill_id INTEGER,
+  link_index INTEGER,
+  parent_state INTEGER,
+  child_state INTEGER,
+  FOREIGN KEY (bill_id) REFERENCES bill(id),
+  FOREIGN KEY (parent_state) REFERENCES bill_state(id),
+  FOREIGN KEY (child_state) REFERENCES bill_state(id)
+);
