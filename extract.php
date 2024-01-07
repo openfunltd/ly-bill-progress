f8faa9d5f275b60954abd6521bc68c9de002c22c
typingmonk <tmonk.hsieh@tmonk.tw>
2024-01-07 21:58:35 +0800

    Remove redundant code

--- extract.php
+++ extract.php
@@ -2,9 +2,6 @@
 
 $files = scandir("./json/");
 $files = array_slice($files, 2);
-$content = file_get_contents("./json/" . $files[0]);
-$json = json_decode($content, true);
-$bills = $json["bills"];
 
 foreach ($files as $file) {
     $bills = get_bills($file);
