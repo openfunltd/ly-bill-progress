d9866f925a3e1e2f71334ba133e35c533983f439
typingmonk <tmonk.hsieh@tmonk.tw>
2024-01-07 22:23:09 +0800

    Get bill_id after insert

--- extract.php
+++ extract.php
@@ -3,12 +3,15 @@
 $files = scandir("./json/");
 $files = array_slice($files, 2);
 
+$pdo = new PDO('sqlite:bill.db');
+
 foreach ($files as $file) {
     $bills = get_bills($file);
     foreach ($bills as $bill) {
         if (!array_key_exists("議案流程", $bill) or count($bill["議案流程"]) == 0) { continue; }
         $progress_array = $bill["議案流程"];
-        list($term, $sessionPeriod, $bill_type, $proposal_source, $billNo, $serial_number) = get_bill_data($bill);
+        $bill_data = get_bill_data($bill);
+        $bill_id = insert_bill_date($pdo, $bill_data);
         foreach ($progress_array as $progress) {
             list($host, $state, $sessionPeriod, $date) = get_progress_data($progress);
         }
@@ -30,6 +33,15 @@ function get_bill_data($bill) {
     $billNo = (array_key_exists("billNo", $bill)) ? $bill["billNo"] : "none";
     $serial_number = (array_key_exists("字號", $bill)) ? $bill["字號"] : "none";
     return array($term, $sessionPeriod, $bill_type, $proposal_source, $billNo, $serial_number);
+}
+
+function insert_bill_date($pdo, $bill_data) {
+    $sql = "INSERT INTO bill " .
+           "(term, session_period, bill_type, proposal_source, ppg_bill_number, serial_number) " .
+           "VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($bill_data);
+    return $pdo->lastInsertId();
 }
 
 function get_progress_data($progress) {
