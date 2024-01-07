e87b9ffea2f5cef5f19b718b81d7eb168b35c5f0
typingmonk <tmonk.hsieh@tmonk.tw>
2024-01-07 21:11:05 +0800

    Read file and get bills data

--- extract.php
+++ extract.php
@@ -1,3 +1,7 @@
 <?php
 
 $files = scandir("./json/");
+$files = array_slice($files, 2);
+$content = file_get_contents("./json/" . $files[0]);
+$json = json_decode($content, true);
+$bills = $json["bills"];
